775d7c88ce0b7954d0bb2c8edfc85fa2865bfea4
Saanvi305 <Saanvimehtauk@gmail.com>
2024-10-23 20:00:29 +0100

    Add files via upload

--- galaga1.py
+++ galaga1.py
@@ -9,17 +9,45 @@ ship=Actor("galaga")
 ship.x=400
 ship.y=520
 
+bullets=[]
+enemies=[]
+enemies.append(Actor("bug"))
+
+
+
 
 def update():
     if keyboard.left:
         ship.x-=5
-        if ship.left<0:
+    if ship.left<0:
             ship.left=0
     elif keyboard.right:
         ship.x+=5
-        if ship.right>WIDTH:
+    if ship.right>WIDTH:
             ship.right=WIDTH
+    for bullet in bullets:
+        if bullet.y<=0:
+             bullets.remove(bullet)
+        bullet.y-=10
+    for enemy in enemies:
+         enemy.y-=12
+         if enemy.y>HEIGHT:
+              enemies.remove(enemy)
+         for bullet in bullets:
+           if enemy.colliderect(bullet):
+                score+100
+                bullets.remove (bullet)
+
             
+
+
+
+
+def on_key_down(key):
+    if key==keys.SPACE:
+          bullets.append(Actor('bullet'))
+          bullets[-1].x=ship.x
+          bullets[-1].y=ship.y-80
 
 
 
@@ -30,6 +58,11 @@ def update():
 def draw():
     screen.clear()
     screen.fill("blue")
+    for bullet in bullets:
+         bullet.draw()
+    for enemy in enemies:
+         enemy.draw()
+
     ship.draw()
 
 
